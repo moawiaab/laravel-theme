563856795b1ed56976c2810c1d27e031b241a712
moawia abudallah <moawiaab@gmail.com>
2023-09-09 11:21:20 +0200

    test ...

--- src/LaravelThemeServiceProvider.php
+++ src/LaravelThemeServiceProvider.php
@@ -39,8 +39,6 @@ class LaravelThemeServiceProvider extends ServiceProvider
         ], 'role-migrations');
 
         copy(__DIR__ . '/Models/User.php', app_path('Models/User.php'));
-        copy(__DIR__ . '/../routes/api.php', base_path('routes/api.php'));
-        // copy(__DIR__ . '/../routes/web.php', base_path('routes/web.php'));
         copy(__DIR__ . '/../database/seeders/DatabaseSeeder.php', database_path('seeders/DatabaseSeeder.php'));
 
         
